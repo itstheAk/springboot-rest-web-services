bf2b15ce1689bb9b8e6244660087ad8ee1750de4
itstheAk <krisananda@gmail.com>
2020-02-18 02:14:34 +0530

    barebone services

--- src/main/java/com/ak/restwebservices/controller/UserController.java
+++ src/main/java/com/ak/restwebservices/controller/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ak.restwebservices.bean.User;
@@ -24,5 +26,10 @@ public class UserController {
 	@GetMapping(path = "/users/{id}")
 	public User findUser(@PathVariable(name = "id") int id) {
 		return userDaoService.findOne(id);
+	}
+	
+	@PostMapping(path = "/users")
+	public void createUser(@RequestBody User user) {
+		userDaoService.save(user);
 	}
 }
